2882c45f68b6f068c9b0181d944a3faa8f3ced6a
Kumar Yashasvi <yashasvi.kumar.sinha@gmail.com>
2020-04-11 21:19:45 +0530

    added show and close modal

--- src/render/renderer.ts
+++ src/render/renderer.ts
@@ -1,1 +1,15 @@
 import './index.css';
+
+const showModal: HTMLElement = document.getElementById('show-modal');
+const closeModal: HTMLElement = document.getElementById('close-modal');
+const modalWindow: HTMLElement = document.getElementById('modal');
+const inputUrl: HTMLElement = document.getElementById('url');
+
+showModal.addEventListener('click', e => {
+    modalWindow.style.display = 'flex';
+    inputUrl.focus();
+});
+
+closeModal.addEventListener('click', e => {
+    modalWindow.style.display = 'none';
+})
